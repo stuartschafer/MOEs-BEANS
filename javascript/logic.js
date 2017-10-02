db708b96db51af7597bc97a5e810ce1fc8606663
Stuart Schafer <stuart_schafer@hotmail.com>
2017-10-02 01:24:05 -0400

    :unamused:Trying to move a pic over

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,49 +1,64 @@
 $(document).ready(function() {
-    let x = "";
-    let y = "";
+    let char = "";
+    let pick = "";
     let charCount = 0;
+    let allCharacters = ["mage", "ogre", "elf"];
+
     $(".moe").hide();
-
-
     $("#messages").html("Please select a character");
+    
+
+
+    
 
 
 
     $("#mage").click(function() {
-        charCount++;
-        x = "picked";
-        y = "mage";
-        $("#ogre").fadeOut("slow");
-        $("#elf").fadeOut("slow");
-        $("#messages").html("Please select a row");
+        pick = "picked";
+        char = "mage";
+        selecChar();
     });
 
     $("#ogre").click(function() {
-        charCount++;
-        x = "picked";
-        y = "ogre";
-        $("#mage").fadeOut("slow");
-        $("#elf").fadeOut("slow");
-        $("#messages").html("Please select a row");
+        pick = "picked";
+        char = "ogre";
+        selecChar();
     });
 
     $("#elf").click(function() {
-        charCount++;
-        x = "picked";
-        y = "elf";
-        $("#mage").fadeOut("slow");
-        $("#ogre").fadeOut("slow");
-        $("#messages").html("Please select a row");
+        pick = "picked";
+        char = "elf";
+        selecChar();
     });
 
     $(".row1").click(function() {
-        if (x === "picked") {
-            $("#" + y + "Row1").fadeIn("slow");
-            $("#" + y).fadeOut("slow");
+        if (pick === "picked") {
+            charCount++;
+            $("#" + char).fadeOut("slow");
             $(".moeChoices").fadeIn("slow");
             $("#messages").html("Please select a character");
-            alert(charCount);
+            pick = "";
+            $("#newChar").html('<img id="' + charCount + char + 'Row1" class="moe" src="images/' + char + '.jpeg"/>');
         }
     });
+
+
+    function selecChar() {
+        if (charCount != 0) {
+            // console.log("#" + charCount + char + "Row1");
+            for (var i=0; i<allCharacters.length; i++) {
+                
+                $("#" + charCount + allCharacters[i] + "Row1").css("margin-left", "20px");
+            }
+        }
+
+
+        for (var i=0; i<allCharacters.length; i++) {
+            if (char != allCharacters[i]) {
+                $("#" + allCharacters[i]).fadeOut("slow");
+            }
+        }
+        $("#messages").html("Please select a row");
+    }
 
 })
